ca871a6176a46794aa772ec99c11a496306a3dae
Jakub Kopacki <jkopacki@gmail.com>
2026-05-15 14:04:57 +0200

    fixed hot reloading in GUI

--- GUI/vite.config.ts
+++ GUI/vite.config.ts
@@ -4,5 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    watch: {
+      usePolling: true,
+      interval: 500,
+    },
+  },
   plugins: [vue(), tailwindcss()],
 })
